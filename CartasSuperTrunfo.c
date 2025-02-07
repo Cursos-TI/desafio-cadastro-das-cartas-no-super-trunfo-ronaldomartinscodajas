830d06416fd0d476955ca018c3c9524e8d4ec31d
ronaldomartinscodajas <ronaldo@freedomnetwork.net.br>
2025-02-07 20:02:42 +0000

    Update Codigo Final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,6 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste Pedro Fontenele
 
 int main(){
     // Cadastro das cartas
